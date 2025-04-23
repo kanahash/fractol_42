eae84f0ad09994577740aa452b2977c132b3112a
Kana Hashimoto <kanahash@c2r3s2.42tokyo.jp>
2025-04-23 16:21:57 +0900

    4

--- src/validate.c
+++ src/validate.c
@@ -6,7 +6,7 @@
 /*   By: kanahash <kanahash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 20:07:42 by kanahash          #+#    #+#             */
-/*   Updated: 2025/04/17 21:02:32 by kanahash         ###   ########.fr       */
+/*   Updated: 2025/04/23 16:20:48 by kanahash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ static int	get_kind_of_fractol(char *str)
 		return (2);
 	return (0);
 }
+
 static int	is_number(char *str)
 {
 	while ((*str >= 9 && *str <= 13) || *str == 32)
